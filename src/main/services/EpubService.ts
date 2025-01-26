4bb08e7d81bced57cfa9c38a02882cd2f6961d3c
Clément <clementfiesta@gmail.com>
2025-01-26 10:20:56 +0100

    EpubService improvement

--- src/main/services/EpubService.ts
+++ src/main/services/EpubService.ts
@@ -3,39 +3,59 @@ import EPub from 'epub';
 
 type EpubMetadata = EPub.Metadata & { publisher?: string; cover?: string };
 
+const IMAGE_REGEX = /src="([^"]+)"/g;
+
 export default class EpubService {
-	private epub: EPub;
+	private static instance: EpubService;
+	private filePath: string;
+	private epub: EPub | null = null;
 
 	constructor(filePath: string) {
-		this.epub = new EPub(filePath);
+		this.filePath = filePath;
+	}
+
+	public static getInstance(filePath: string): EpubService {
+		if (
+			!EpubService.instance ||
+			EpubService.instance.filePath !== filePath
+		) {
+			this.instance.close();
+			EpubService.instance = new EpubService(filePath);
+		}
+		return EpubService.instance;
 	}
 
 	private async initialize(): Promise<void> {
+		if (this.epub) return;
+
+		this.epub = new EPub(this.filePath);
+
 		return new Promise((resolve, reject) => {
-			this.epub.on('end', resolve);
-			this.epub.on('error', reject);
-			this.epub.parse();
+			this.epub!.on('end', resolve);
+			this.epub!.on('error', (error) => {
+				this.epub = null;
+				reject(error);
+			});
+			this.epub!.parse();
 		});
+	}
+
+	async close(): Promise<void> {
+		if (this.epub) {
+			this.epub.removeAllListeners();
+			this.epub = null;
+		}
 	}
 
 	async extractMetadata(): Promise<Omit<Book, 'id' | 'path' | 'format'>> {
 		await this.initialize();
 
+		if (!this.epub) return Promise.reject('Epub not initialized');
+
 		const metadata: EpubMetadata = this.epub.metadata;
 
 		let cover = null;
-		if (metadata.cover)
-			cover = await new Promise<string | null>((resolve) => {
-				this.epub.getImage(metadata.cover!, (err, data, mimeType) => {
-					if (err) {
-						resolve(null);
-					} else {
-						const base64Image = `data:${mimeType};base64,${data.toString('base64')}`;
-						resolve(base64Image);
-					}
-				});
-			});
-
+		if (metadata.cover) cover = await this.getImageBase64(metadata.cover);
 		return {
 			title: metadata.title,
 			cover: cover,
@@ -50,5 +70,69 @@ export default class EpubService {
 			coverage: null,
 			source: null,
 		};
+	}
+
+	async extractTOC(): Promise<{ title: string; href: string }[]> {
+		await this.initialize();
+
+		if (!this.epub) return Promise.reject('Epub not initialized');
+
+		return this.epub.toc.map((item) => ({
+			title: item.title,
+			href: item.href,
+		}));
+	}
+
+	async getChapter(href: string): Promise<string> {
+		await this.initialize();
+
+		if (!this.epub) return Promise.reject('Epub not initialized');
+
+		return new Promise((resolve, reject) => {
+			this.epub!.getChapter(href, (err, text) => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(text);
+				}
+			});
+		});
+	}
+
+	async getImageBase64(href: string): Promise<string> {
+		await this.initialize();
+
+		if (!this.epub) return Promise.reject('Epub not initialized');
+
+		return new Promise((resolve, reject) => {
+			this.epub!.getImage(href, (err, data, mimeType) => {
+				if (err) {
+					reject(err);
+				} else {
+					const base64Image = `data:${mimeType};base64,${data.toString('base64')}`;
+					resolve(base64Image);
+				}
+			});
+		});
+	}
+
+	async getFormattedChapter(href: string): Promise<string> {
+		const rawContent = await this.getChapter(href);
+
+		let match;
+		let formattedContent = rawContent;
+
+		while ((match = IMAGE_REGEX.exec(rawContent)) !== null) {
+			const imagePath = match[1];
+			const imageBase64 = await this.getImageBase64(imagePath);
+			if (imageBase64) {
+				formattedContent = formattedContent.replace(
+					match[0],
+					`src="${imageBase64}"`,
+				);
+			}
+		}
+
+		return formattedContent;
 	}
 }
