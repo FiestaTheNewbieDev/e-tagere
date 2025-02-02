841ae78b688cab726ec2f1d466b463eab9e42053
Clément <clementfiesta@gmail.com>
2025-02-02 12:10:34 +0100

    Improve book.date type

---
+++ prisma/migrations/20250202100407_convert_book_date_into_string/migration.sql
@@ -0,0 +1,26 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_book" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "path" TEXT NOT NULL,
+    "format" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "cover" TEXT,
+    "author" TEXT,
+    "language" TEXT,
+    "identifier" TEXT,
+    "publisher" TEXT,
+    "subject" TEXT,
+    "description" TEXT,
+    "date" TEXT,
+    "rights" TEXT,
+    "coverage" TEXT,
+    "source" TEXT
+);
+INSERT INTO "new_book" ("author", "cover", "coverage", "date", "description", "format", "id", "identifier", "language", "path", "publisher", "rights", "source", "subject", "title") SELECT "author", "cover", "coverage", "date", "description", "format", "id", "identifier", "language", "path", "publisher", "rights", "source", "subject", "title" FROM "book";
+DROP TABLE "book";
+ALTER TABLE "new_book" RENAME TO "book";
+CREATE UNIQUE INDEX "book_path_key" ON "book"("path");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
